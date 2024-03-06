ddecc5f23843c03a22c882701f98032ea987dc2c
Kinshuk Dwivedi <007kinshuk@gmail.com>
2024-03-07 01:35:28 +0530

    skeleton graphs are working fine

---
+++ src/Components/Card.js
@@ -0,0 +1,84 @@
+import React from "react";
+import React from "react";
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+const LineChart = () => {
+  return (
+    <div>
+      <Line data={data} />
+    </div>
+  );
+};
+const convert = (timestamp) => {
+  const d = new Date(timestamp);
+  const hour = d.getHours();
+  const minutes = d.getMinutes();
+  const time = hour + ":" + minutes;
+  return time;
+};
+const Card = (props) => {
+  console.log("IamProps");
+  const d = props.graphLines;
+  const x = d?.[0]?.values;
+  const y = d?.[1]?.values;
+  const z = d?.[2]?.values;
+  const valuesArrayX = x ? x.map((obj) => obj.value) : [];
+  const valuesArrayY = y ? y.map((obj) => obj.value) : [];
+  const valuesArrayZ = z ? z.map((obj) => obj.value) : [];
+  const valuesTimes = x ? x.map((obj) => convert(obj.timestamp)) : [];
+  // console.log(valuesArray);
+  // console.log("break");
+  // console.log(valuesTimes);
+  const data = {
+    labels: valuesTimes,
+    datasets: [
+      {
+        label: "Used",
+        backgroundColor: "rgb(220, 38, 38)",
+        borderColor: "rgb(220, 38, 38)",
+        data: valuesArrayX,
+        tension: 0.08,
+        borderWidth: 2,
+        pointRadius: 0,
+      },
+      {
+        label: "Requested",
+        backgroundColor: "rgb(37,99,235)",
+        borderColor: "rgb(37,99,235)",
+        data: valuesArrayY,
+        tension: 0.08,
+        borderWidth: 2,
+        pointRadius: 0,
+      },
+      {
+        label: "Limits",
+        backgroundColor: "rgb(5,150,105)",
+        borderColor: "rgb(5,150,105)",
+        data: valuesArrayZ,
+        tension: 0.1,
+        borderWidth: 2,
+        pointRadius: 0,
+      },
+    ],
+  };
+  const options = {
+    plugins: {
+      legend: {
+        display: false, // Hide legend labels and colors
+      },
+      layout: {
+        padding: {
+          left: 20,
+          right: 20,
+        },
+      },
+    },
+  };
+  return (
+    <div>
+      <Line data={data} options={options} />
+    </div>
+  );
+};
+
+export default Card;
--- src/Components/Metrics.js
+++ src/Components/Metrics.js
@@ -1,35 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MimicMetrics } from "../API";
+import Card from "./Card";
+import Area from "./Area";
 const Metrics = () => {
-  //   const now = Date.now();
-  //   const fiveMinutesAgo = now - 5 * 60 * 1000; // 5 minutes ago
-  //   MimicMetrics.fetchMetrics({ startTs: fiveMinutesAgo, endTs: now })
-  //     .then((metrics) => {
-  //       console.log(metrics);
-  //       // Do something with the metrics data
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching metrics:", error);
-  //     });
+  const [data, setData] = useState([]);
+
   const fetchData = async () => {
     try {
       const now = Date.now();
-      const fiveMinutesAgo = now - 5 * 60 * 1000; // 5 minutes ago
+      // const fiveMinutesAgo = now - 5 * 60 * 1000; // 5 minutes ago
+      const oneHourAgo = now - 60 * 60 * 1000;
       const metrics = await MimicMetrics.fetchMetrics({
-        startTs: fiveMinutesAgo,
+        startTs: oneHourAgo,
         endTs: now,
       });
-      console.log(metrics);
+      setData(metrics);
       // Do something with the metrics data
     } catch (error) {
       console.error("Error fetching metrics:", error);
     }
   };
 
-  fetchData();
+  useEffect(() => {
+    fetchData();
+  }, []); // Empty dependency array ensures that fetchData is called only once when the component mounts
+
   return (
-    <div className="bg-slate-300">
-      <div className="bg-white m-4"> Metrics {" date + time"} </div>
+    <div className="items-center">
+      <h1 className="text-l font-semibold m-2">Metrics</h1>
+      <p className="text-sm">
+        {new Date().toLocaleDateString("en-GB")} {" -> "}{" "}
+        {new Date(Date.now() - 5 * 60 * 1000).toLocaleDateString("en-GB")}
+      </p>
+      <div>
+        <Card {...data[0]} />
+        <Card {...data[1]} />
+        <Card {...data[2]} />
+        {/* <Area {...data[3]} /> */}
+      </div>
     </div>
   );
 };
